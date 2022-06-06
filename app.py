23592b76146dd9fefd9f6c48ea95ce78577ef1a7
Conor Power <conorgpower@gmail.com>
2022-06-06 19:08:57 +0100

    Finish all sections

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
+fa = FontAwesome(app)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# app.run(host='127.0.0.1', port=8080)
